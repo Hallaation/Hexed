a8b630dc4012276b41830c30412f693ed3bd9570
Lincoln Gardner <lincoln.gardner@hotmail.com>
2017-07-26 14:39:15 +1000

    Teleport guy

--- Assets/Scripts/Abilities/Teleport.cs
+++ Assets/Scripts/Abilities/Teleport.cs
@@ -1,25 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XboxCtrlrInput;
+using XInputDotNetPure;
 
 public class Teleport : BaseAbility
 {
     [Space]
+    public float m_TeleportForce;
     [Header("Teleport Variables")]
     public float test;
-    public float m_fMaximumMana = 100;
-    public float PassiveManaRegeneration = 1.0f;
+   
+    Rigidbody2D _rigidBody;
+    ControllerSetter m_controller;
 
-    public float ManaCost = 50.0f;
     // Use this for initialization
     void Start()
     {
+        m_TeleportForce = 10;
+        m_fMaximumMana = 50f;
+        PassiveManaRegeneration = 1;
+        ManaCost = 50f;
+        m_controller = GetComponent<ControllerSetter>();
+        _rigidBody = GetComponent<Rigidbody2D>();
     }
     
     // Update is called once per frame
     void Update()
     {
-
+        if (XCI.GetButton(XboxButton.DPadUp, m_controller.mXboxController))
+        {
+            _rigidBody.position += new Vector2(transform.up.x * m_TeleportForce, transform.up.y * m_TeleportForce);
+            _rigidBody.AddForce(transform.up * 10);
+        }
     }
 
     void UseSpecialAbility()
