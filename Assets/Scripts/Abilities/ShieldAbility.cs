850c6a770431c86f820a36d226b0e99369288a28
Lincoln Gardner <lincoln.gardner@hotmail.com>
2017-08-03 16:03:15 +1000

    Bullets and guns now use continous collision. bullets update now obsolete. Added Door Prefab. bullets now interact with everything in the enviroment including other bullets.

--- Assets/Scripts/Abilities/ShieldAbility.cs
+++ Assets/Scripts/Abilities/ShieldAbility.cs
@@ -9,74 +9,77 @@ public class ShieldAbility : BaseAbility
     [Header("Shield Variables")]
     public float testVariable;
     [SerializeField]
-    public GameObject shieldObject;
+    private GameObject shieldObject;
 
     bool m_bPoweredUp = false;
     bool PowerReset = false;
     // Use this for initialization
     public override void Initialise()
-    {
+
+
+    { m_bPoweredUp = true;
         if (findUI)
         {
             shieldObject = transform.Find("ShieldSprite").gameObject;
-            shieldObject.SetActive(false);
-        }
-    }
-
-    public override void UseSpecialAbility(bool UsingAbility)
-    {
-        //if the input calls for a power usage and the power hasnt been reset yet (trigger hasnt been let go yet)
-        //once I want to use the ability, power reset is true
-        if (UsingAbility)
-        {
-            if (currentMana >= m_fMinimumManaRequired)
-            {
-                PowerReset = true;
-            }
-        }
-
-        if (UsingAbility && PowerReset)
-        {
-            if (currentMana >= m_fMaximumMana)
-            {
-                m_bPoweredUp = true;
-            }
-            RegenMana = false;
-            currentMana -= repeatedManaCost * Time.deltaTime;
-            this.GetComponent<Move>().HideWeapon(true);
             shieldObject.SetActive(true);
         }
-        //once the left trigger is at value 0, power reset is fals;e
-        else
-        {
-            this.GetComponent<Move>().HideWeapon(false);
-            PowerReset = false;
-            shieldObject.SetActive(false);
-            m_bPoweredUp = false;
-        }
-
-        //TODO Shield logic goes here.
     }
 
-    public override void AdditionalLogic()
-    {
-        if (shieldObject)
-        {
-            if (!shieldObject.activeInHierarchy)
-            {
-                RegenMana = true;
-            }
-        }
+    //public override void UseSpecialAbility(bool UsingAbility)
+    //{
 
-        if (currentMana <= 0)
-        {
-            PowerReset = false;
-        }
-        if (_AbilityTypeText)
-        {
-            _AbilityTypeText.text = "Ability : Shield";
-        }
-    }
+    //    //if the input calls for a power usage and the power hasnt been reset yet (trigger hasnt been let go yet)
+    //    //once I want to use the ability, power reset is true
+    //    if (UsingAbility)
+    //    {
+    //        if (currentMana >= m_fMinimumManaRequired)
+    //        {
+    //            PowerReset = true;
+    //        }
+    //    }
+
+    //    if (UsingAbility && PowerReset)
+    //    {
+    //        if (currentMana >= m_fMaximumMana)
+    //        {
+    //            m_bPoweredUp = true;
+    //        }
+    //        RegenMana = false;
+    //        currentMana -= repeatedManaCost * Time.deltaTime;
+    //        this.GetComponent<Move>().HideWeapon(true);
+    //        shieldObject.SetActive(true);
+    //    }
+    //    //once the left trigger is at value 0, power reset is fals;e
+    //    else
+    //    {
+    //        this.GetComponent<Move>().HideWeapon(false);
+    //        PowerReset = false;
+    //        shieldObject.SetActive(false);
+    //        m_bPoweredUp = false;
+    //    }
+
+    //    //TODO Shield logic goes here.
+    //}
+
+    //public override void AdditionalLogic()
+    //{
+    //    if (shieldObject)
+    //    {
+    //        if (!shieldObject.activeInHierarchy)
+    //        {
+    //            RegenMana = true;
+    //        }
+    //    }
+
+    //    if (currentMana <= 0)
+    //    {
+    //        PowerReset = false;
+    //    }
+    //    if (_AbilityTypeText)
+    //    {
+    //        _AbilityTypeText.text = "Ability : Shield";
+    //    }
+    //}
 
     public void TakeBullet(GameObject bullet , RaycastHit2D rayHit)
     {
@@ -86,7 +89,7 @@ public class ShieldAbility : BaseAbility
             bullet.transform.rotation = Quaternion.Inverse(bullet.transform.rotation);
 
             Vector3 velocity = bullet.GetComponent<Rigidbody2D>().velocity;
-            bullet.GetComponent<Rigidbody2D>().velocity = Vector3.Reflect(bullet.transform.up + velocity * 0.5f , rayHit.transform.up);
+            bullet.GetComponent<Rigidbody2D>().velocity = Vector3.Reflect(bullet.transform.up + velocity , rayHit.transform.up);
             bullet.GetComponent<Bullet>().bulletOwner = null;
         }
         else
