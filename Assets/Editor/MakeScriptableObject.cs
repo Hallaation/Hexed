d7b1c101695f7f542c9660410a49b1f7d1ecba85
Louis Nguyen <nguyenlouis4@gmail.com>
2017-08-31 14:39:56 +1000

    Animator null checks, Playercolour start. Map sprite fix
    Started making player colour editor.
    Fixed map sprite in the gamemode selection scirpt.
    Fixed missing script on map prefab.

---
+++ Assets/Editor/MakeScriptableObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MakeScriptableObject
+{
+
+    [MenuItem("Assets/Create/PlayerColours")]
+    public static void CreatePlayerColourAsset()
+    {
+        PlayerColours playercolours = ScriptableObject.CreateInstance<PlayerColours>();
+        AssetDatabase.CreateAsset(playercolours, "Assets/Resources/PlayerColours.asset");
+        AssetDatabase.SaveAssets();
+    }
+
+}
