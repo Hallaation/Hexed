7343cf204903a851373851231a3d6a00ca7d668b
Lincoln Gardner <lincoln.gardner@hotmail.com>
2017-10-20 16:32:30 +1100

    Sound added for headsmash.

--- Assets/Scripts/HeadSmashSound.cs
+++ Assets/Scripts/HeadSmashSound.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class HeadSmashSound : StateMachineBehaviour {
     public AudioClip HeadSmasheeSound;
     protected AudioSource m_AudioSource;
-
+   
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        //m_AudioSource = 
 
+/*        m_AudioSource.Play()*/
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
--- Assets/Scripts/Sounds/PlayHeadSmashSound.cs
+++ Assets/Scripts/Sounds/PlayHeadSmashSound.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class PlayHeadSmashSound : MonoBehaviour {
     public AudioClip HeadSmasheeSound;
     AudioSource audio;
+    public bool Play;
     // Use this for initialization
     void Start () {
-        audio = GetComponent<AudioSource>();
-        audio.Play(44100);
-       
+        
+      
+      
     }
 	
+    void Awake()
+    {
+        
+    }
+
+   public void PlaySound()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.outputAudioMixerGroup = (Resources.Load("AudioMixer/SFXAudio") as GameObject).GetComponent<AudioSource>().outputAudioMixerGroup;
+        audio.Play();
+    }
 	// Update is called once per frame
 	void Update () {
 		
