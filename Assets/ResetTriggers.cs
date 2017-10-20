7776a2c7179c3293d39d614ba0d44cc9349d10de
Lincoln Gardner <lincoln.gardner@hotmail.com>
2017-10-20 12:23:51 +1100

    Changes to animator. Fixed some bugs some may still remain.

--- Assets/ResetTriggers.cs
+++ Assets/ResetTriggers.cs
@@ -6,7 +6,13 @@ public class ResetTriggers : StateMachineBehaviour {
 
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        animator.ResetTrigger("CancelHeadSmash");
+        
+        animator.ResetTrigger("HavingHeadSmashed");
+        animator.ResetTrigger("HavingHeadSmashedPullUp");
+        animator.ResetTrigger("HeadSmashPullUp");
+        animator.ResetTrigger("HeadSmashSmash");
+
+        animator.SetBool("CancelHeadSmash", false);
     }
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
