dd38dbaedb4d13dad8763ebb53033e65976c4c73
Louis Nguyen <nguyenlouis4@gmail.com>
2017-10-27 14:38:14 +1100

    New changes

--- Assets/Scripts/UI/SpriteSelection.cs
+++ Assets/Scripts/UI/SpriteSelection.cs
@@ -3,18 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SpriteSelection : MonoBehaviour {
+public class SpriteSelection : MonoBehaviour
+{
 
     public Sprite[] m_Sprites;
     private Image m_SRenderer;
-	// Use this for initialization
-	void Start ()
+
+    public float lowerLimit = 0;
+    public float upperlimit = 1;
+    // Use this for initialization
+    void Start()
     {
-        m_SRenderer = this.GetComponent<Image>();	
-        if (m_Sprites.Length > 0)
+        m_SRenderer = this.GetComponent<Image>();
+        StartCoroutine(ChangeSprites());
+    }
+
+
+    IEnumerator ChangeSprites()
+    {
+        while (true)
         {
-            m_SRenderer.sprite = m_Sprites[Random.Range(0, m_Sprites.Length)];
+            yield return new WaitForSeconds(Random.Range(lowerLimit, upperlimit));
+            if (m_Sprites.Length > 0)
+            {
+                m_SRenderer.sprite = m_Sprites[Random.Range(0, m_Sprites.Length)];
+            }
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+            if (m_Sprites.Length > 0)
+            {
+                m_SRenderer.sprite = m_Sprites[0];
+            }
+            yield return null;
         }
-	}
+    }
 
 }
